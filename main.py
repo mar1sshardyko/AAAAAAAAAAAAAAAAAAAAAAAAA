46a52def5034f0dcb2255c1a75872cf505c7d925
maris <maris12345mimi@gmail.com>
2022-11-19 12:20:36 +0100

    uraaaaaa

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ opener = urllib.request.build_opener()
 response =  opener.open ("https://vk.com/")
 print (response.read())
 print("=======================================")
+#кхурс битка
 import requests
-response = requests.get('https://vk.com/')
-print (type(response.content))
-
+response = requests.get('https://coinmarketcap.com/')
+response_text = response.text.split("<span>")
+for parse_elem_1 in response_parse:
+        if parse_elem_1.startswith('$'):
+            #print(parse_elem_1)
+            for parse_elem_2 in parse_elem_1.split('</span>'):
+                    if parse_elem_2.startswith('$') and parse_elem_2[1].isdigit():
+                            print(parse_elem_2)
