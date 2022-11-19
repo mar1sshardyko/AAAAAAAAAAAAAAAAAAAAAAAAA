bb9bbeadb0cb553362d822137ceeb044f38d2aa6
maris <maris12345mimi@gmail.com>
2022-11-19 12:56:46 +0100

    opa opa opa

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ print (response.read())
 print("=======================================")
 #кхурс битка
 import requests
+res_parse_list = []
 response = requests.get('https://coinmarketcap.com/')
 response_text = response.text.split("<span>")
 for parse_elem_1 in response_parse:
@@ -12,4 +13,6 @@ for parse_elem_1 in response_parse:
             #print(parse_elem_1)
             for parse_elem_2 in parse_elem_1.split('</span>'):
                     if parse_elem_2.startswith('$') and parse_elem_2[1].isdigit():
-                            print(parse_elem_2)
+                            res_parse_list.append(parse_elem_2)
+bitok = res_parse_list[0]
+print(bitok)
